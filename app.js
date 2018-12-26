9023bcf1a072d3a3d4ffe38ade822b7d7a40a3dd
emmadeas <emmadeas9@gmail.com>
2018-12-26 11:53:51 -0800

    Working routes

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
-const express = require("express"); 
-const app = express(); 
+const express = require("express");
+const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Kumasaka")); 
+
+
+
+
+app.get("/", (req, res) => res.send("Hello Kuma")); 
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`)); 
+app.listen(port, () => console.log(`Server is running on port ${port}`));
